074db030796bfa737ea4aec77da91b74528b9dbe
dancancro <dan.cancro@gmail.com>
2017-06-08 23:55:46 -0400

    wip

--- src/main/webapp/app/features/dashboard/dashboard.page.ts
+++ src/main/webapp/app/features/dashboard/dashboard.page.ts
@@ -1,18 +1,26 @@
-import { Component } from '@angular/core';
-import { JhiLanguageService } from 'ng-jhipster';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { EventManager, ParseLinks, PaginationUtil, JhiLanguageService, AlertService } from 'ng-jhipster';
+import { Subscription } from 'rxjs/Rx';
 
 @Component({
     selector: 'jhi-dashboard',
     styleUrls: ['./dashboard.page.scss'],
     templateUrl: './dashboard.page.html'
 })
-export class DashboardPage {
+export class DashboardPage implements OnDestroy {
+    eventSubscriber: Subscription;
     // true here is for using subdirectories, you can also specify regex as third param
     // public pathToImages = require.context('../../../', true, /\.(jpe?g|png|gif|svg|woff2?|ttf|eot)$/i);
 
     constructor(
-        private jhiLanguageService: JhiLanguageService) {
+        private eventManager: EventManager,
+        // private jhiLanguageService: JhiLanguageService
+    ) {
         // this.jhiLanguageService.setLocations(['all']);
 
+    }
+
+    ngOnDestroy() {
+        this.eventManager.destroy(this.eventSubscriber);
     }
 }
